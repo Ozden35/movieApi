81018b62c11df7eec8c8ecc8a647ef9254355d4b
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2025-04-29 21:27:23 +0300

    #20 Movie Api - AspNet Core 9.0 Onion Architecture - Mediator Tasarım Deseninde Movie Tag İşlemleri

--- Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagByIdQuery.cs
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagByIdQuery.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.TagResults;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries
 {
-    public class GetTagByIdQueryResult :IRequest<GetTagByIdQueryResult>
+    public class GetTagByIdQuery : IRequest<GetTagByIdQueryResult>
     {
         public int TagId { get; set; }
 
-        public GetTagByIdQueryResult(int tagId)
+        public GetTagByIdQuery(int tagId)
         {
             TagId = tagId;
         }
