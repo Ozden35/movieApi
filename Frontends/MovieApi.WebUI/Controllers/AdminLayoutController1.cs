25107e1413bf9538d25ace837cf7927c329e8ab5
Ozden35 <115698170+Ozden35@users.noreply.github.com>
2025-05-01 23:15:58 +0300

    #23 Movie Api - AspNet Core 9.0 Onion Architecture - UI Projesinin Oluşturulması

---
+++ Frontends/MovieApi.WebUI/Controllers/AdminLayoutController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class AdminLayoutController1 : Controller
+    {
+        public IActionResult LayoutUI()
+        {
+            return View();
+        }
+    }
+}
